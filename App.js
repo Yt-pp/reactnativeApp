b2deba00ac18bfa334892a24e60a6858172e0f53
yutong <yutong.teo@beans.com.my>
2024-02-27 23:33:35 +0800

    push

---
+++ App.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+import { View,Text } from 'react-native';
+import AppNavigation from './navigation/appNavigation';
+
+
+function App() {
+  
+
+  return (
+   <AppNavigation></AppNavigation>
+  );
+}
+
+
+export default App;
